65f0e05284af44f1704bd1f5d17d9870a6036465
S0RGE <vitaliynaf@gmail.com>
2023-01-11 03:25:48 +0700

    table init

---
+++ src/components/TableProduct.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th v-for="theader in theaders" :key="theader">{{ theader }}</th>
+      </tr>
+    </thead>
+    <tbody>
+      <product-line v-for="product in products" :key="product.id" />
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+import ProductLine from './ProductLine.vue';
+
+export default {
+  props: {
+    theaders: {
+      type: Array,
+      default: ['Number', 'Name', 'Description', 'Price'],
+    },
+    products: {
+      type: Array,
+    },
+  },
+  components: {
+    ProductLine,
+  },
+};
+</script>
+
+<style></style>
